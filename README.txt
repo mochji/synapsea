77001b574734ce94672e13e11f29ccb0f3b7e34f
xxoa <117334318+x-xxoa@users.noreply.github.com>
2023-02-18 21:03:03 -0500

    it's done, 10 hours of straight programming.
    im in pain

---
+++ README.txt
@@ -0,0 +1,96 @@
+Lua: https://lua.org
+
+(oh boy here we go)
+
+    ::::DOCUMENTATION::::
+
+the 'lnn.asserttype()' function has 3 parameters: variable (the variable you want to check the type of), variablename (the variable name for more verbose error messages) and thetype
+(the type of variable you want to confirm it is). if the variable type is equal to the type of variable you want it to be it will continue the code, if not it will produce an error
+message with information about the variable.
+
+the 'lnn.assertsize()' function has 4 parameters: a (the first table), b (the second table), aname (the name of the first table, for more verbose error messages) and bname (the name
+of the second table (for more verbose error messages). if the size of a is equal to the size of b it will continue the code, if not it will produce an error message with information
+about the table sizes.
+
+the 'lnn.findintable()' function has 2 parameters: item (the item you want to find in the table) and table (the table you want to find item in). it does what it sounds like, if it
+finds item in table it will return true, if not it will return false.
+
+the 'lnn.sigmoid()' function has 2 parameters: x (the number you want to put into the sigmoid function) and derivative (if you want the derivative of the sigmoid function). if the
+derivative parameter is true it returns x put into the derivative of the sigmoid function and if it's false it returns x put into the sigmoid function.
+
+the 'lnn.tanh()' function has 2 parameters: x (the number you want to put into the tanh function) and derivative (if you want the derivative of the tanh function). if the
+derivative parameter is true it returns x put into the derivative of the tanh function and if it's false it returns x put into the tanh function.
+
+the 'lnn.relu()' function has 2 parameters: x (the number you want to put into the relu function) and derivative (if you want the derivative of the relu function). if the
+derivative parameter is true it returns x put into the derivative of the relu function and if it's false it returns x put into the relu function.
+
+the 'lnn.leakyrelu()' function has 2 parameters: x (the number you want to put into the leakyrelu function) and derivative (if you want the derivative of the leakyrelu function).
+if the derivative parameter is true it returns x put into the derivative of the leakyrelu function and if it's false it returns x put into the leakyrelu function.
+
+the 'lnn.initialize()' function has 5 parameters: id (the id for the neural network), activation (the activation function for the neual network), insize (the size of the inputs for
+the neural network) layercount (the amount of layers for the neural network) and outcount (the size of the output nodes for the neural network). it creates the data values for the
+neural network, the weights, biases and current values. the amount of nodes in each layer is calculated from a 2 point slope, (y2-y1)/(x1-x2), where y2 is the output node count, y1
+is the input node count, x1 is 0 and x2 is the layercount, the amount of hidden layers in the neural network.
+
+the 'lnn.forwardpass()' function has 2 parameters: id (the id for the neural network) and intable (the input for the neural network). it propagates the input forward through the
+neural network with the activation function as specified from the 'lnn.initialize()' function. it returns a table even if the amount of output nodes is 1. 
+ 
+the 'lnn.adjust()' function has 5 parameters: id (the id for the neural network), intable (the input table to adjust the weights and biases with respect to), out (the real output
+from the neural network), expectedout (the expected or ideal output from the neural network) and learningrate (usually a low value like 0.01 or 0.001). it adjusts the weights and
+the biases by calculating gradw and gradb then subtracting gradw from the weights and subtracting gradb from the biases.
+
+the 'lnn.getmse()' function has 2 parameters: output (the real output from the neural network) and expectedoutput (the expected or ideal output from the neural network). the functiom
+returns the mean squared error calculated from the output and expected output.
+
+the 'lnn.getsse()' function has 2 parameters: output (the real output from the neural network) and expectedoutput (the expected or ideal output from the neural network). the functiom
+returns the sum of squared error calculated from the output and expected output.
+
+the 'lnn.getrmse)' function has 2 parameters: output (the real output from the neural network) and expectedoutput (the expected or ideal output from the neural network). the functiom
+returns the root of mean squared error calculated from the output and expected output.
+
+the 'lnn.getcrossentropy()' function has 2 parameters: output (the real output from the neural network) and expectedoutput (the expected or ideal output from the neural network). the
+function returns the cross entropy value calculated from the output and expected output. 
+
+the 'lnn.debug.returnweights()' function has 1 parameter: id (the id for the neural network). the function returns a table with the weight values for the neural network. this can
+be used for visualizing or debugging.
+
+the 'lnn.debug.returnbiases()' function has 1 parameter: id (the id for the neural network). the function returns a table with the bias values for the neural network. this can be
+used for visualizing or debugging.
+
+the 'lnn.debug.returncurrent()' function has 1 parameter: id (the id for the neural network). the function returns a table with the current values (the node value of each layer) for
+the neural network. this can be used for visualizing or debugging.
+
+the 'lnn.debug.returnwgradient()' function has 1 parameter: id (the id for the neural network). the function returns a table with the gradient values for the neural network. this can
+be used for visualizing or debugging. (or you can just use _G[id][gradient][gradw or gradb])
+
+the 'lnn.debug.returndata()' function has 1 parameter: id (the id for the neural network). the function returns the table with the data for the neural network (info like layercount,
+outcount, ect). (or you can just use _G[id])
+
+    ::::HOW THE NEURAL NETWORK WORKS::::
+
+the neural networks starts by creating the functions, the first and only function created is getlayer(). getlayer() takes the last layer, the next layer, the weight layer inbetween
+and the bias table for the next layer. it then declares 1 variable, sum, and loops for the amount of items in the nextlayer (#nextlayer, loop variable is a). then in that loop there
+is another loop that loops for the amount of items in the lastlayer (#lastlayer, loop variable is i), then it adds the i'th item in lastlayer multiplied by the i+(a-1) multiplied by
+the amount of items in the last layer (#lastlayer). then the a'th item in the next layer (nextlayer) is set to sum + the current node's bias (biases[a]) put into the activation
+function of choice.
+
+the input is fed into the neural network in the form of a table, this is because it's easy to work with tables and if you want to read an image like BMP or PNG you would have to
+write code to convert it to a table (or use https://github.com/Didericis/png-lua#pnglua :D) getlayer() is called for the input and the first hidden layer and then a loop for the
+amount of hidden layers starting at 2 (loop variable is i) calls getlayer() for the last layer (_G[id.."c"..i-1]) and the next layer (_G[id.."c"..i]). once the loop is done it calles
+getlayer() again for the lastlayer in the hidden layers and the output nodes. it then returns the output (_G[id.."o"]).
+
+the adjust function starts by checking if out and expected out are the same size to prevent an error, it then gets the weighted sum of the weights and puts it into the derivative of
+the activation function. before it does anything else it checks if the averaged mse for the output is below a certain value to avoid overfitting and if it is then return true. it then
+calculates the gradient descent table for the weights and then the gradient descent table for the biases, it adjusts the output layer weights based on gradw and then the rest of the
+weights, also based on gradw. after that it adjusts the output biases based on gradb, and then adjusts the rest of the biases, also based on gradb.
+
+    ::::POTENTIAL OPTIMIZATIONS::::
+
+some potential optimizations i copied from myself when i was at school during lunch at 11:05 am on thursday, febuary 16th, 2023 are are all for the back-propagation algorithm. there
+are optimizations that are possible for the forward-pass algorithm but i'm not able to come up with any right now. after the output weights are adjusted it then adjusts the rest of
+the weights, then again on the next gradw item, then again and so on. this can be optimized by subtracting all of them together and then subtracting the weights from that, same with
+the biases.
+
+another potential optimization is having a dataset parameter for the lnn.adjust() function. the dataset parameter could be a table with a list of inputs, outputs and expected outputs.
+
+thats it i'm done with this it's 8:57 pm saturday febuary 18th 2023 and i've worked on this code and documentation for 10 hours straight i'll make it a luarocks package soon.
---
+++ main.lua
@@ -0,0 +1,530 @@
+--READ THE README.txt FILE FOR DOCUMENTATION AND OTHER STUFF!!!!
+
+--health update: i've been working on this for 5 hours straight, everything looks like it's slightly slanted.
+--health update 2: 8th hour, feeling better than ever.
+
+_G["lnn"] = {}
+_G["lnn"]["debug"] = {}
+
+function lnn.asserttype(variable,variablename,thetype)
+    --check for errors in the function that checks for errors.
+    if type(thetype)  ~= "string" or type(variablename) ~= "string" then
+        error("variablename and thetype must be a string.")
+    end
+
+    --give an error if false or nil
+    if type(variable) ~= thetype or variable == nil then
+        string.format("%s (%s) is not a %s or is nil. Type: %s", variablename, tostring(variable), thetype, type(variable))
+    end
+end
+
+function lnn.assertsize(a,b,aname,bname)
+    --check for errors in the function that checks for errors but different.
+    if type(a) ~= "table" or type(b) ~= "table" then
+        error("a and b must be a table.")
+    end
+    if type(aname) ~= "string" or type(bname) ~= "string" then
+        error("aname and bname must be a string.")
+    end
+
+    --give an error theyr'e not the same size.
+    if #a ~= #b then
+        error(aname.." ("..#a..") is not the same size as "..bname.." ("..#b..").")
+    end
+end
+
+function lnn.findintable(item,table)
+    --check for errors
+    lnn.asserttype(table,"table","table")
+
+    --do the stuff
+    for i = 1,#table do
+        if table[i] == item then
+            return true
+        end
+    end
+    return false
+end
+
+function lnn.sigmoid(x,derivative)
+    --check for errors
+    lnn.asserttype(x,"x","number")
+    lnn.asserttype(derivative,"derivative","boolean")
+
+    --do the stuff
+    if derivative then
+        return (1 / (1 + math.exp(-x))) * (1-(1 / (1 + math.exp(-x))))
+    else
+        return 1 / (1 + math.exp(-x))
+    end
+end
+
+function lnn.tanh(x,derivative)
+    --check for errors
+    lnn.asserttype(x,"x","number")
+    lnn.asserttype(derivative,"derivative","boolean")
+
+    --do the stuff
+    if derivative then
+        return 1 - (((math.exp(2*x) - 1)/(math.exp(2*x) + 1))^2)
+    else
+        return (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+    end
+end
+
+function lnn.relu(x,derivative)
+    --check for errors
+    lnn.asserttype(x,"x","number")
+    lnn.asserttype(derivative,"derivative","boolean")
+
+    --do the stuff
+    if derivative then
+        if x < 0 then
+            return 0
+        else
+            return 1
+        end
+    else
+        return math.max(0,x)
+    end
+end
+
+function lnn.leakyrelu(x,derivative)
+    --check for errors
+    lnn.asserttype(x,"x","number")
+    lnn.asserttype(derivative,"derivative","boolean")
+
+    --do the stuff
+    if derivative then
+        if x > 0 then
+            return 1
+        else
+            return 0.01
+        end
+    else
+        if x > 0 then
+            return x
+        else
+            return x*0.01
+        end
+    end
+end
+
+function lnn.initialize(id,activation,intable,layercount,outcount)
+    --check for errors
+    lnn.asserttype(id,"id","string")
+    lnn.asserttype(activation,"activation","string")
+    lnn.asserttype(intable,"intable","table")
+    lnn.asserttype(layercount,"layercount","number")
+    lnn.asserttype(outcount,"outcount","number")
+
+    --check if the id already exists
+    if _G[id] ~= nil then
+        error("id ("..id..") already exists.")
+    end
+
+    --available activation functions
+    local activationtable = {"sig","tanh","relu","lrelu"}
+    
+    --check if the activation is a valid activation function
+    if not lnn.findintable(activation,activationtable) then
+        error(activation.." is not a valid activation function, available activation functions are: 'sig', 'tanh', 'relu' and 'lrelu'.")
+    end
+
+    --initialize the neural network
+
+    --initialize the neural network data
+    _G[id] = {}
+    _G[id]["activation"] = activation
+    _G[id]["layercount"] = layercount
+    _G[id]["outcount"] = outcount
+    _G[id]["insize"] = #intable
+    _G[id]["gradient"] = {}
+    _G[id]["gradient"]["gradw"] = {}
+    _G[id]["gradient"]["gradb"] = {}
+    _G[id]["id"] = id
+
+    --initialize the neural network layers
+    
+    local amounttofill = 0
+    
+    --check if layercount is 0
+    if layercount == 0 then
+        --create the tables for the output weights
+        _G[id.."ow"] = {}
+        for i = 1,#intable*outcount do
+            _G[id.."ow"][i] = math.random(0,100)/100
+        end
+
+        --create the values for the output node values (bias and current)
+        _G[id.."ob"] = {}
+        _G[id.."o"] = {}
+        for i = 1,#outcount do
+            _G[id.."ob"] = math.random(0,100)/100
+            _G[id.."o"] = 0
+        end
+        
+        return
+    end
+
+    --create the tables for the node values (bias and current)
+    for i = 1,layercount do
+        local ctablename = id.."c"..i
+        local btablename = id.."b"..i
+        _G[ctablename] = {}
+        _G[btablename] = {}
+        
+        amounttofill = math.ceil(((outcount - #intable) * i / (layercount - 0)) + #intable)
+
+        for a = 1,amounttofill do
+            _G[ctablename][a] = 0.0
+            _G[btablename][a] = math.random(0,100)/100
+        end
+    end
+
+    --create the tables for the connection values (weight)
+    for i = 1,layercount + 1 do
+        local wtablename = id.."w"..i
+        _G[wtablename] = {}
+        
+        if i > 1 then --get the amount to fill
+            amounttofill = math.ceil(((outcount - #intable) * (i - 1) / (layercount - 0)) + #intable)*math.ceil(((outcount - #intable) * i / (layercount - 0)) + #intable)
+        else
+            amounttofill = #intable*math.ceil(((outcount - #intable) * i / (layercount - 0)) + #intable)
+        end
+
+        for a = 1,amounttofill do
+            _G[wtablename][a] = math.random(0,100)/100
+        end
+    end
+    
+    --create the tables for the output (bias and current)
+    _G[id.."ob"] = {}
+    _G[id.."o"] = {}
+    for i = 1,outcount do
+        _G[id.."ob"][i] = math.random(0,100)/100
+        _G[id.."o"][i] = 0.0
+    end
+
+    --create the tables for the output connection (weight)
+    _G[id.."ow"] = {}
+    for i = 1,outcount*math.ceil(((outcount - #intable) * layercount / (layercount - 0)) + #intable) do
+        _G[id.."ow"][i] = math.random(0,100)/100
+    end
+end
+
+function lnn.forwardpass(id,intable)
+    --check if the id doesn't exist
+    if _G[id] == nil then
+        error("id ("..id..") doesnt exist.")
+    end
+    
+    --check for errors
+    if #intable ~= _G[id]["insize"] then
+        error("intable ("..#intable..") is not the same size as the intable when id ("..id..") was initialized (".._G[id][4]").")
+    end
+    lnn.asserttype(id,"id","string")
+    lnn.asserttype(intable,"intable","table")
+    
+    --declare the functions
+    local function getlayer(lastlayer,nextlayer,weights,biases)
+        --declare the variables
+        local sum = 0
+
+        --get the sum of the connected weights to the current node we are on and replace the nextlayer
+        if _G[id]["activation"] == "sig" then
+            for a = 1,#nextlayer do
+                for i = 1,#lastlayer do
+                    sum = sum + lastlayer[i]*(weights[i+((a-1)*#lastlayer)])
+                end
+                nextlayer[a] = lnn.sigmoid(-sum+biases[a])
+                sum = 0
+            end
+        elseif _G[id]["activation"] == "tanh" then
+            for a = 1,#nextlayer do
+                for i = 1,#lastlayer do
+                    sum = sum + lastlayer[i]*(weights[i+((a-1)*#lastlayer)])
+                end
+                nextlayer[a] = lnn.tanh(-sum+biases[a])
+                sum = 0
+            end
+        elseif _G[id]["activation"] == "relu" then
+            for a = 1,#nextlayer do
+                for i = 1,#lastlayer do
+                    sum = sum + lastlayer[i]*(weights[i+((a-1)*#lastlayer)])
+                end
+                nextlayer[a] = lnn.relu(-sum+biases[a])
+                sum = 0
+            end
+        elseif _G[id]["activation"] == "lrelu" then
+            for a = 1,#nextlayer do
+                for i = 1,#lastlayer do
+                    sum = sum + lastlayer[i]*(weights[i+((a-1)*#lastlayer)])
+                end
+                nextlayer[a] = lnn.leakyrelu(-sum+biases[a])
+                sum = 0
+            end
+        end
+    end
+
+    --do the stuff
+
+    --check if layercount is 0
+    if _G[id]["layercount"] == 0 then
+        getlayer(intable,_G[id.."o"],_G[id.."ow"],_G[id.."ob"])
+        return _G[id.."o"]
+    end
+    
+    --if there's hidden layers
+    getlayer(intable,_G[id.."c1"],_G[id.."w1"],_G[id.."b1"]) --input layer to first hidden
+    for i = 2,_G[id]["layercount"],1 do --rest of the hidden layers
+        getlayer(_G[id.."c"..i-1],_G[id.."c"..i],_G[id.."w"..i],_G[id.."b"..i])
+    end
+    getlayer(_G[id.."c".._G[id]["layercount"]],_G[id.."o"],_G[id.."ow"],_G[id.."ob"])
+
+    return _G[id.."o"]
+end
+
+function lnn.adjust(id,intable,out,expectedout,learningrate)
+    --check for errors
+    lnn.asserttype(id,"id","string")
+    lnn.asserttype(intable,"intable","table")
+    lnn.asserttype(out,"out","table")
+    lnn.asserttype(expectedout,"expectedout","table")
+    lnn.asserttype(learningrate,"learningrate","number")
+    
+    lnn.assertsize(out,expectedout,"out","expectedout")
+    if _G[id] == nil then
+        error("id ("..id..") doesn't exist.")
+    end
+    if _G[id]["insize"] ~= #intable then
+        error("insize (".._G[id]["insize"]..") is not the same size as intable ("..#intable..").")
+    end
+
+    --declare the variables
+    local gradw = {}
+    local gradb = {}
+    local weightedsum = 0
+    local da_wsum = 0
+
+    --get the sum of the weighted inputs
+    for a = 1,#_G[id.."c1"] do
+        for i = 1,#intable do
+            weightedsum = weightedsum + _G[id.."w1"][i+((a-1)*#intable)]*intable[i]
+        end
+    end
+
+    --get gradw
+    for i = 1,#out do
+        gradw[i] = ((out[i]-expectedout[i])^2*da_wsum)*learningrate+((out[i]-expectedout[i])*learningrate)
+    end
+
+    --get gradb
+    for i = 1,#out do
+        gradb[i] = ((expectedout[i]-out[i])*da_wsum)*learningrate+((expectedout[i]-out[i])*learningrate)
+    end
+
+    --update the data on _G[id]
+    _G[id]["gradient"]["gradw"] = gradw
+    _G[id]["gradient"]["gradb"] = gradb
+
+    --get da_wsum
+    if _G[id]["activation"] == "sig" then
+        da_wsum = lnn.sigmoid(weightedsum,true)
+    elseif _G[id]["activation"] == "tanh" then
+        da_wsum = lnn.tanh(weightedsum,true)
+    elseif _G[id]["activation"] == "relu" then
+        da_wsum = lnn.relu(weightedsum,true)
+    elseif _G[id]["activation"] == "lrelu" then
+        da_wsum = lnn.leakyrelu(weightedsum,true)
+    end
+    
+    --adjust weights
+
+    --adjust the output layer weights
+    for a = 1,#out do
+        for i = 1,#_G[id.."c".._G[id]["layercount"]] do
+            _G[id.."ow"][i+((a-1)*#_G[id.."c".._G[id]["layercount"]])] = _G[id.."ow"][i+((a-1)*#_G[id.."c".._G[id]["layercount"]])] - gradw[i]
+        end
+    end
+
+    --adjust the rest of the weights
+    for a = 1,#out do
+        for b = _G[id]["layercount"],1,-1 do
+            for i = 1,#_G[id.."w"..b] do
+                _G[id.."w"..b][i] = _G[id.."w"..b][i] - gradw[a]
+            end
+        end
+    end
+
+    --adjust biases
+
+    --adjust the output layer biases
+    for i = 1,#out do
+        _G[id.."ob"][i] = _G[id.."ob"][i] - gradb[i]
+    end
+
+    --adjust the rest of the biases
+    for a = 1,#out do
+        for b = _G[id]["layercount"],1,-1 do
+            for i = 1,#_G[id.."b"..b] do
+                _G[id.."b"..b][i] = _G[id.."b"..b][i] - gradb[a]
+            end
+        end
+    end
+end
+
+function lnn.getmse(output,expectedoutput)
+    --check for errors
+    lnn.asserttype(output,"output","table")
+    lnn.asserttype(expectedoutput,"expectedutput","table")
+
+    lnn.assertsize(#output,#expectedoutput,"output","expectedoutput")
+
+    --declare the variables
+    local mse = 0
+
+    --do the stuff
+    for i = 1,#output do
+         mse = mse + (expectedoutput[i] - output[i])^2
+    end
+    return mse/#output
+end
+
+function lnn.getsse(output,expectedoutput)
+    --check for errors
+    lnn.asserttype(output,"output","table")
+    lnn.asserttype(expectedoutput,"expectedutput","table")
+
+    lnn.assertsize(#output,#expectedoutput,"output","expectedoutput")
+
+    --declare the variables
+    local sse = 0
+
+    --do the stuff
+    for i = 1,#output do
+        sse = (output[i]-expectedoutput[i])^2
+    end
+    return sse/#output
+end
+
+function lnn.getrmse(output,expectedoutput)
+    --check for errors
+    lnn.asserttype(output,"output","table")
+    lnn.asserttype(expectedoutput,"expectedutput","table")
+
+    lnn.assertsize(#output,#expectedoutput,"output","expectedoutput")
+
+    --declare the variables
+    local rmse = 0
+
+    --do the stuff
+    for i = 1,#output do
+        rmse = rmse + (expectedoutput[i] - output[i])^2
+    end
+
+    return math.sqrt((rmse/#output))
+end
+
+function lnn.getcrossentropy(output,expectedoutput)
+    --check for errors.
+    lnn.asserttype(output,"output","table")
+    lnn.asserttype(expectedoutput,"expectedoutput","table")
+
+    lnn.assertsize(output,expectedoutput,"output","expectedoutput")
+
+    --declare the variables
+    local sum = 0
+
+    --do the stuff
+    for i = 1,#output do
+        sum = sum + (expectedoutput[i]*math.log(output[i])) + (1-expectedoutput[i]) * math.log(1-output[i])
+    end
+    return -sum
+end
+
+--either debugging or visualizing, could be used for both.
+
+function lnn.debug.returnweights(id)
+    --check for errors.
+    lnn.asserttype(id,"id","string")
+
+    if _G[id] == nil then
+        error("id ("..id..") doesn't exist.")
+    end
+
+    --declare the variables
+    local returntable = {}
+
+    --do the stuff
+    for i = 1,_G[id]["layercount"] do
+        returntable[i] = table.pack(_G[id.."w"..i])
+    end
+    returntable[#returntable+1] = table.pack(id.."ow")
+
+    return returntable
+end
+
+function lnn.debug.returnbiases(id)
+    --check for errors.
+    lnn.asserttype(id,"id","string")
+
+    if _G[id] == nil then
+        error("id ("..id..") doesn't exist.")
+    end
+
+    --declare the variables
+    local returntable = {}
+
+    --do the stuff
+    for i = 1,_G[id]["layercount"] do
+        returntable[i] = table.pack(_G[id.."b"..i])
+    end
+    returntable[#returntable+1] = table.pack(id.."ob")
+
+    return returntable
+end
+
+function lnn.debug.returncurrent(id)
+    --check for errors.
+    lnn.asserttype(id,"id","string")
+
+    if _G[id] == nil then
+        error("id ("..id..") doesn't exist.")
+    end
+
+    --declare the variables
+    local returntable = {}
+
+    --do the stuff
+    for i = 1,_G[id]["layercount"] do
+        returntable[i] = table.pack(_G[id.."c"..i])
+    end
+    returntable[#returntable+1] = table.pack(id.."o")
+
+    return returntable
+end
+
+function lnn.debug.returngradient(id)
+    --check for errors.
+    lnn.asserttype(id,"id","string")
+
+    if _G[id] == nil then
+        error("id ("..id..") doesn't exist.")
+    end
+
+    --do the stuff
+    return _G[id]["gradient"]
+end
+
+function lnn.debug.returndata(id)
+    --check for errors.
+    lnn.asserttype(id,"id","string")
+
+    if _G[id] == nil then
+        error("id ("..id..") doesn't exist.")
+    end
+
+    --do the stuff
+    return _G[id]
+end

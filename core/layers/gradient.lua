2484e50721f121a3614e8b51300b03a0dd516748
mochji <owo.mochji@gmail.com>
2023-11-12 21:41:33 -0500

    small

--- core/layers/gradient.lua
+++ core/layers/gradient.lua
@@ -25,6 +25,46 @@
 	bias = bias - lr * error_sum * input
 ]]--
 
-local gradientModule = {}
+local gradientModule = {
+	dense,
+	averagePooling1D,
+	averagePooling2D,
+	averagePooling3D,
+	maxPooling1D,
+	maxPooling2D,
+	maxPooling3D,
+	sumPooling1D,
+	sumPooling2D,
+	sumPooling3D,
+	upSample1D,
+	upSample2D,
+	upSample3D,
+	zeroPad1D,
+	zeroPad2D,
+	zeroPad3D,
+	crop1D,
+	crop2D,
+	crop3D,
+	convolutional1D,
+	convolutional2D,
+	convolutional3D,
+	flatten,
+	reshape,
+	add1D,
+	add2D,
+	add3D,
+	subtract1D,
+	subtract2D,
+	subtract3D,
+	multiply1D,
+	multiply2D,
+	multiply3D,
+	divide1D,
+	divide2D,
+	divide3D,
+	softmax,
+	activate,
+	dropOut
+}
 
 return gradientModule
